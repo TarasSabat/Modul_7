b07d07fded02c91ad456bb77e07ef8a544949348
TarasSabat <tarsab25@gmail.com>
2023-10-19 21:33:49 +0300

    1

---
+++ hello_world_stv/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='hello_world_stv',
+    version='0.0.1',
+    description='Very useful code',
+    author='Taras',
+    author_email='flyingcircus@example.com',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
+    packages=find_namespace_packages(),
+    entry_points={'console_scripts': ['greeting=hello_world_stv.main:greeting']})
--- konspekt_package/main.py
+++ konspekt_package/main.py
@@ -1,15 +1,18 @@
-from my_package import *
+from konspekt_package.foo import foo
+from konspekt_package.baz.operation import mul, sum
+from konspekt_package.bar.info import log
 
-def mul(a):
-    return a * 5
+
+# def mul(a):
+#     return a * 5
 
 
-if __name__ == "__main__":
-    print(foo('Natalia'))
-    print(sum(1, 4))
-    # print(mul(5, 6))
-    # log('hello world')
-    print(mul(5))
-    print(foo('Test'))
-    # get_value()
+# if __name__ == "__main__":
+print(foo('Natalia'))
+print(sum(1, 4))
+print(mul(5, 6))
+log('hello world')
+# print(mul(5))
+# print(foo('Test'))
+# get_value()
 

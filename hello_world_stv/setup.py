16965fee8679fb090a371a44b2cb1f1cff149c21
TarasSabat <tarsab25@gmail.com>
2023-10-20 22:45:28 +0300

    1

--- hello_world_stv/setup.py
+++ hello_world_stv/setup.py
@@ -4,13 +4,9 @@ setup(
     name='hello_world_stv',
     version='0.0.1',
     description='Very useful code',
-    author='Taras',
+    author='Taras Sabat',
     author_email='flyingcircus@example.com',
     license='MIT',
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     packages=find_namespace_packages(),
-    entry_points={'console_scripts': ['greeting=hello_world_stv.main:greeting']})
+    entry_points={'console_scripts': ['greeting=hello_world_stv.main:greeting']}
+)

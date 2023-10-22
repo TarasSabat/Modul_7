9f0c5f0ebdffac886be67eed8f879f0d5676308f
TarasSabat <tarsab25@gmail.com>
2023-10-22 13:28:40 +0300

    1

--- Home_work_clean/clean_folder_STV/setup.py
+++ Home_work_clean/clean_folder_STV/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name='clean_folder',
-    version='0.0.2',
+    name='clean_folder_STV',
+    version='0.0.1',
     author='Taras Sabat',
     packages=find_namespace_packages(),
-    entry_points={'console_scripts':['clean=clean_folder.clean:start']}
+    entry_points={'console_scripts':['clean=clean_folder_STV.clean:start']}
 )
